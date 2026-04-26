8bbdac9c9ce38832712900bf3f2e59429d066c80
Jayden Phom <70143012+Aphite@users.noreply.github.com>
2026-04-25 20:31:36 -0400

    Attack animation/code added. Tweaked jump animation.

--- Assets/scripts/PlayerAttack.cs
+++ Assets/scripts/PlayerAttack.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     
+    public Animator animator;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -15,7 +16,7 @@ public class PlayerAttack : MonoBehaviour
     void Attack()
     {
         // Attack Animation
-
+        animator.SetTrigger("attack");
         // Detect enemies in range and apply damage
 
         // Damage enemies
