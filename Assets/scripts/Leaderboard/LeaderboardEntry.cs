bb37eefe85372fe7f08369b94a643083ddb00926
Jayden Phom <70143012+Aphite@users.noreply.github.com>
2026-04-28 02:44:16 -0400

    Save and loading of leaderboard works now. No save progress or loading.
    - Saves stats of the run upon going to the main menu or upon reaching the required amount of goals to win.

--- Assets/scripts/Leaderboard/LeaderboardEntry.cs
+++ Assets/scripts/Leaderboard/LeaderboardEntry.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System;
 
 [Serializable]
-public class LeaderboardEntry : MonoBehaviour
+public class LeaderboardEntry
 {
     public string playerName;
     public int    score;
